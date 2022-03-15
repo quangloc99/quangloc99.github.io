8b1ce8a6785f9eba6bac59348d99871ba76e0b6c
Tran Quang Loc <quangloctran99@gmail.com>
2022-03-15 18:17:22 +0300

    Add curtain for dynamic skin

--- assets/js/dynamic-skin.js
+++ assets/js/dynamic-skin.js
@@ -1,31 +1,52 @@
 // the trick is described here:
 // https://derekkedziora.com/blog/dark-mode
 
-const theme = localStorage.getItem('theme');
-if (theme === "dark") {
-    document.documentElement.setAttribute('data-theme', 'dark');
-}
-if (!theme) {
-    const userPrefers =
-        getComputedStyle(document.documentElement).getPropertyValue('content');
+{
+    const curtainMovingTime_ms = 0.2 * 1000;
+    const backgroundChangingTime_ms = 0.4 * 1000;
 
-    if (userPrefers === "dark") {
+    const theme = localStorage.getItem('theme');
+    if (theme === "dark") {
         document.documentElement.setAttribute('data-theme', 'dark');
-        window.localStorage.setItem('theme', 'dark');
-    } else {
-        document.documentElement.setAttribute('data-theme', 'light');
-        window.localStorage.setItem('theme', 'light');
+    }
+    if (!theme) {
+        const userPrefers = getComputedStyle(document.documentElement)
+                                .getPropertyValue('content');
+
+        if (userPrefers === "dark") {
+            document.documentElement.setAttribute('data-theme', 'dark');
+            window.localStorage.setItem('theme', 'dark');
+        } else {
+            document.documentElement.setAttribute('data-theme', 'light');
+            window.localStorage.setItem('theme', 'light');
+        }
     }
 
-}
-    
-function modeSwitcher() {
-    let currentMode = document.documentElement.getAttribute('data-theme');
-    if (currentMode === "dark") {
-        document.documentElement.setAttribute('data-theme', 'light');
-        window.localStorage.setItem('theme', 'light');
-    } else {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        window.localStorage.setItem('theme', 'dark');
+    const curtain = document.createElement("div");
+    curtain.classList.add("curtain");
+    window.addEventListener("load", () => document.body.appendChild(curtain));
+
+    let isCovering = false;
+
+    window.modeSwitcher = function modeSwitcher() {
+        if (isCovering)
+            return;
+        isCovering = true;
+        curtain.classList.add("covering");
+        setTimeout(() => {
+            let currentMode =
+                document.documentElement.getAttribute('data-theme');
+            if (currentMode === "dark") {
+                document.documentElement.setAttribute('data-theme', 'light');
+                window.localStorage.setItem('theme', 'light');
+            } else {
+                document.documentElement.setAttribute('data-theme', 'dark');
+                window.localStorage.setItem('theme', 'dark');
+            }
+            setTimeout(() => {
+                curtain.classList.remove("covering");
+                setTimeout(() => isCovering = false, curtainMovingTime_ms);
+            }, backgroundChangingTime_ms);
+        }, curtainMovingTime_ms);
     }
 }
